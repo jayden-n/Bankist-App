ce979fd9f6558f120d894263abf3dba460b604e5
jayden-n <jaydennguyen.dev@gmail.com>
2022-11-18 21:04:16 -0500

    Implementing Blur input field

--- script.js
+++ script.js
@@ -132,6 +132,10 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
     // Display movements
     displayMovements(currentAccount.movements);
     // Display balance
