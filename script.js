d7a4cb4ecae8c358a9fb8f6e3aecaba317117997
jayden-n <jaydennguyen.dev@gmail.com>
2022-11-19 09:49:00 -0500

    Implementing the request loan feature

--- script.js
+++ script.js
@@ -169,6 +169,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
