73b29f84a6bfe9b28f4843b6c2679d29e5e50580
jayden-n <jaydennguyen.dev@gmail.com>
2022-11-23 20:12:00 -0500

    LECTURE: setTimeout Method

--- script.js
+++ script.js
@@ -751,3 +751,13 @@ console.log(
   new Intl.NumberFormat(navigator.language).format(num)
 );
 */
+
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
