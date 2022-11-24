f30b5e895691d81f2d539649640d2662f37d5041
jayden-n <jaydennguyen.dev@gmail.com>
2022-11-23 20:26:22 -0500

    LECTURE: setInterval Method

--- script.js
+++ script.js
@@ -754,6 +754,7 @@ console.log(
 );
 */
 
+// setTimeout
 const ingredients = ['olives', 'spinach'];
 const pizzaTimer = setTimeout(
   (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
@@ -763,3 +764,9 @@ const pizzaTimer = setTimeout(
 console.log('Waiting...');
 
 if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 8000);
